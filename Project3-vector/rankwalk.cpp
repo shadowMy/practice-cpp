757427eb111a183ad31e39b2e3051bbc8caa7167
shadowMy <943749527@qq.com>
2020-03-01 19:14:32 +0800

    添加一个文件输出

--- Project3-vector/rankwalk.cpp
+++ Project3-vector/rankwalk.cpp
@@ -2,17 +2,21 @@
 #include<cstdlib>	//rand(), srand()原型prototypes
 #include<ctime>		//time()原型prototype
 #include "vector.h"
+////	#include<fstream>
 int main()
 {
 	using namespace std;
 	using VECTOR::Vector;
-	srand(time(0));
+	srand(time(0));					//srand()、time()
 	double direction;	//方向 方位
 	Vector step;		//步 一步 脚步
 	Vector result(0.0, 0.0);
 	unsigned long steps = 0;
+////	unsigned long i = 0;
 	double target;		//目标 靶子
 	double dstep;
+////	ofstream fout;
+////	fout.open("test.txt");
 	cout << "Enter target distance (q to quit): ";
 	while (cin >> target)
 	{
@@ -22,9 +26,11 @@ int main()
 
 		while (result.magval() < target)
 		{
-			direction = rand() % 360;
+			direction = rand() % 360;					//rand()
 			step.reset(dstep, direction, Vector::POL);
 			result = result + step;
+////			fout << i << ": " << result << endl;
+////			i++;
 			steps++;
 		}
 		cout << "After " << steps << "steps, the subject has the following location:\n";
@@ -33,6 +39,7 @@ int main()
 		cout << " or\n" << result << endl;
 		cout << "Average outward distance per step = " << result.magval() / steps << endl;
 		steps = 0;
+////		i = 0;
 		result.reset(0.0, 0.0);
 		cout << "Enter target distance(q to quit): ";
 	}
